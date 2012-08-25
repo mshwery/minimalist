16151f86a9f6a40db540146a50ee1a60898c1942
Matt Shwery <mshwery@gmail.com>
2012-08-25 13:06:33 -0500

    add list and stack totals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     @body_classes ||= ["#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"]
     @body_classes
   end
+
+  def list_total
+    List.all.count
+  end
+
+  def stack_total
+    Stack.all.count
+  end
 end
