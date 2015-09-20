ef636a7b64b5f8df96b5da05a44b91c60d598be7
Matt Shwery <mshwery@gmail.com>
2015-09-19 19:09:49 -0500

    upgrade to last rails 3 version

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Lists::Application.config.session_store :cookie_store, key: '_Lists_session'
+Lists::Application.config.session_store :cookie_store, key: '_lists_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
